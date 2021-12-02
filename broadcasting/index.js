74ee92cfc2dc7b0663e099c38b2cda4a11a61428
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-12-02 18:28:19 +0600

    Use of namespace for making separation between communication channels

--- broadcasting/index.js
+++ broadcasting/index.js
@@ -7,9 +7,22 @@ const expressServer = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(expressServer);   //made socket server with respect to expressServer
 
-io.on("connection", function(socket){
-    io.sockets.emit('MyBroatcast', "First code with Broatcast in Socket.io!")
+// -----> Create Namespace connection
+let buyNsp = io.of("/buy");
+buyNsp.on('connection', function(socket){
+    buyNsp.emit("MyBroatcast", "I am from Buy Namespace!")
 })
+
+let sellNsp = io.of("/sell");
+sellNsp.on('connection', function(socket){
+    sellNsp.emit("MyBroatcast", "I am from Sell Namespace!")
+})
+
+
+
+// io.on("connection", function(socket){
+//     io.sockets.emit('MyBroatcast', "First code with Broatcast in Socket.io!")
+// })
 
 
 app.get('/', function(req, res){ 
